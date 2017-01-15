01aee909297a3a9b8330feea281548fcebe7add4
Daniel Quah <danielquah148@yahoo.com>
2017-01-15 15:22:01 +0800

    Database integration(WIP)

--- app/src/main/java/org/sp/attendance/ats/nearby/reboot/signInActivity.java
+++ app/src/main/java/org/sp/attendance/ats/nearby/reboot/signInActivity.java
@@ -1,10 +1,12 @@
 package org.sp.attendance.ats.nearby.reboot;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -14,21 +16,28 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 /**
  * Created by Daniel on 26/12/2016.
  */
 
-public class signInActivity extends AppCompatActivity{
+public class signInActivity extends AppCompatActivity {
 
     private final int REQUEST_PERMISSION_RECORD_AUDIO=1;
-
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         setContentView(R.layout.activity_signin);
         super.onCreate(savedInstanceState);
         showPermission();
+        context = this;
+        String ats_database_url = "ats.nearby.com";
+        CheckUrl url = new CheckUrl();
+        url.execute(ats_database_url);
     }
 
     public void signIn(View view) {
@@ -40,10 +49,7 @@ public class signInActivity extends AppCompatActivity{
                     .setTitle(R.string.title_sign_in_failed)
                     .setMessage(R.string.error_credentials_disappeared)
                     .setCancelable(false)
-                    .setPositiveButton(R.string.dismiss, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                        }
+                    .setPositiveButton(R.string.dismiss, (dialog,which) -> {
                     })
                     .create()
                     .show();
@@ -81,15 +87,13 @@ public class signInActivity extends AppCompatActivity{
     }
 
     private void showExplanation(String title, String message, final String permission, final int permissionRequestCode) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(title)
+        new AlertDialog.Builder(this)
+                .setTitle(title)
                 .setMessage(message)
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        requestPermission(permission, permissionRequestCode);
-                    }
-                });
-        builder.create().show();
+                .setPositiveButton(android.R.string.ok,(dialog,which) -> requestPermission(permission, permissionRequestCode))
+                .create()
+                .show();
+
     }
 
     private void requestPermission(String permissionName, int permissionRequestCode) {
@@ -97,4 +101,46 @@ public class signInActivity extends AppCompatActivity{
                 new String[]{permissionName}, permissionRequestCode);
     }
 
+    private class CheckUrl extends AsyncTask<String, Void, Boolean> {
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute(){
+            progressDialog = ProgressDialog.show(signInActivity.this,"","Please wait",true);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params){
+            try{
+                HttpURLConnection.setFollowRedirects(false);
+                HttpURLConnection con = (HttpURLConnection) new URL(params[0]).openConnection();
+                con.setRequestMethod("HEAD");
+                return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
+            } catch (Exception e){
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result){
+            boolean bResponse = result;
+            if(bResponse){
+                progressDialog.dismiss();
+            } else {
+                new AlertDialog.Builder(context)
+                        .setTitle(R.string.error)
+                        .setMessage(R.string.click_here)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.here, (dialog,which) ->
+                        {
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://github.com/emansih/ATS_Backend"));
+                            startActivity(browserIntent);
+                            System.exit(0);
+                        })
+                        .create()
+                        .show();
+
+            }
+        }
+    }
 }
