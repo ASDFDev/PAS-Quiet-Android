ASDFDev/PAS-Quiet-Android

commit cdd878a72366a9e85760771b924a2814aa562528

Refractor code

app/src/main/java/com/setsuna/client/quiet/lecturer/ReceiveActivity.java
@@ -1,10 +1,9 @@
-package com.setsuna.client.quiet;
+package com.setsuna.client.quiet.lecturer;
 
 
-import android.content.Intent;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import java.nio.charset.Charset;
@@ -22,28 +21,34 @@ import rx.subscriptions.Subscriptions;
 import android.provider.Settings.Secure;
 import android.widget.Toast;
 
+import com.setsuna.client.quiet.util.ActivityUtil;
+import com.setsuna.client.quiet.util.FrameReceiverObservable;
+import com.setsuna.client.quiet.util.ModelSerializer;
+import com.setsuna.client.quiet.R;
 import com.setsuna.client.quiet.util.AccountManager;
 import com.setsuna.client.quiet.util.ApiManager;
 import com.setsuna.client.quiet.util.PermissionManager;
+import com.setsuna.client.quiet.util.ReceiveUtil;
 
 
 public class ReceiveActivity extends AppCompatActivity {
 
     private Subscription frameSubscription = Subscriptions.empty();
-
+    private Context context;
+    private ReceiveUtil receiveUtil = new ReceiveUtil(context);
+    private PermissionManager permissionManager = new PermissionManager(context);
+    private ActivityUtil activityUtil = new ActivityUtil(context);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_receive);
         super.onCreate(savedInstanceState);
-        askPermission();
-    }
-
-    private void askPermission(){
-        PermissionManager permissionManager = new PermissionManager(this);
         permissionManager.showPermission();
     }
 
+    public ReceiveActivity(Context context){
+        this.context = context;
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
@@ -55,15 +60,13 @@ public class ReceiveActivity extends AppCompatActivity {
                     subscribeToFrames();
                 } else {
                     Toast.makeText(this, R.string.no_permission, Toast.LENGTH_LONG).show();
-                    Intent signInIntent = new Intent(this, signInActivity.class);
-                    this.startActivity(signInIntent);
-
+                    activityUtil.goToSignIn();
                 }
         }
     }
 
     private void subscribeToFrames() {
-        frameSubscription = FrameReceiverObservable.create(this, "ultrasonic-experimental").subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(buf -> {
+        frameSubscription = FrameReceiverObservable.create(this, context.getResources().getString(R.string.quiet_profile)).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(buf -> {
 
             frameSubscription.unsubscribe();
             Retrofit retrofit = new Retrofit.Builder()
@@ -81,30 +84,11 @@ public class ReceiveActivity extends AppCompatActivity {
             databaseModelCall.enqueue(new Callback<ModelSerializer>() {
                 @Override
                 public void onResponse(Call<ModelSerializer> call, Response<ModelSerializer> response) {
-                    new AlertDialog.Builder(ReceiveActivity.this)
-                            .setTitle("Attendance submission failed!")
-                            .setMessage("You have already submitted attendance on this device")
-                            .setCancelable(false)
-                            .setPositiveButton(R.string.ok, (dialog,which) -> {
-                                finish();
-                                Intent signInIntent = new Intent(ReceiveActivity.this,signInActivity.class);
-                                ReceiveActivity.this.startActivity(signInIntent);
-                            })
-                            .create()
-                            .show();
+                    receiveUtil.showAttendanceResults("Attendance Submission Failed!", "You have already submitted attendance on this device");
                 }
                 @Override
                 public void onFailure(Call<ModelSerializer> call, Throwable t) {
-                    new AlertDialog.Builder(ReceiveActivity.this)
-                            .setTitle("Attendance Submitted!")
-                            .setCancelable(false)
-                            .setPositiveButton(R.string.ok, (dialog,which) -> {
-                                finish();
-                                Intent signInIntent = new Intent(ReceiveActivity.this,signInActivity.class);
-                                ReceiveActivity.this.startActivity(signInIntent);
-                            })
-                            .create()
-                            .show();
+                    receiveUtil.showAttendanceResults("Attendance Submitted!" , "");
                 }
             });
         }, error-> {

commit 00a9352f4fe7b9b430648a4d45160681ed13698a

Bug fixed - Fix NullPointerException introduced in cdd878a - Update README(point to website) - Switch lecturer to receiver - Switch student to broadcaster

app/src/main/java/com/setsuna/client/quiet/lecturer/ReceiveActivity.java
@@ -1,9 +1,12 @@
 package com.setsuna.client.quiet.lecturer;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import java.nio.charset.Charset;
@@ -25,34 +28,29 @@ import com.setsuna.client.quiet.util.ActivityUtil;
 import com.setsuna.client.quiet.util.FrameReceiverObservable;
 import com.setsuna.client.quiet.util.ModelSerializer;
 import com.setsuna.client.quiet.R;
-import com.setsuna.client.quiet.util.AccountManager;
-import com.setsuna.client.quiet.util.ApiManager;
-import com.setsuna.client.quiet.util.PermissionManager;
-import com.setsuna.client.quiet.util.ReceiveUtil;
+import com.setsuna.client.quiet.util.manager.AccountManager;
+import com.setsuna.client.quiet.util.manager.ApiManager;
+import com.setsuna.client.quiet.util.manager.PermissionManager;
 
 
 public class ReceiveActivity extends AppCompatActivity {
 
+    private Activity activity;
     private Subscription frameSubscription = Subscriptions.empty();
     private Context context;
-    private ReceiveUtil receiveUtil = new ReceiveUtil(context);
-    private PermissionManager permissionManager = new PermissionManager(context);
-    private ActivityUtil activityUtil = new ActivityUtil(context);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_receive);
         super.onCreate(savedInstanceState);
+        PermissionManager permissionManager = new PermissionManager(this);
         permissionManager.showPermission();
     }
 
-    public ReceiveActivity(Context context){
-        this.context = context;
-    }
-
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode,@NonNull String permissions[], @NonNull int[] grantResults) {
         final int REQUEST_PERMISSION_RECORD_AUDIO = 1;
+        ActivityUtil activityUtil = new ActivityUtil(this);
         super.onRequestPermissionsResult(requestCode,permissions,grantResults);
         switch (requestCode) {
             case REQUEST_PERMISSION_RECORD_AUDIO:
@@ -66,8 +64,7 @@ public class ReceiveActivity extends AppCompatActivity {
     }
 
     private void subscribeToFrames() {
-        frameSubscription = FrameReceiverObservable.create(this, context.getResources().getString(R.string.quiet_profile)).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(buf -> {
-
+        frameSubscription = FrameReceiverObservable.create(ReceiveActivity.this, this.getResources().getString(R.string.quiet_profile)).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(buf -> {
             frameSubscription.unsubscribe();
             Retrofit retrofit = new Retrofit.Builder()
                     .addConverterFactory(GsonConverterFactory.create())
@@ -84,15 +81,25 @@ public class ReceiveActivity extends AppCompatActivity {
             databaseModelCall.enqueue(new Callback<ModelSerializer>() {
                 @Override
                 public void onResponse(Call<ModelSerializer> call, Response<ModelSerializer> response) {
-                    receiveUtil.showAttendanceResults("Attendance Submission Failed!", "You have already submitted attendance on this device");
+                    showAttendanceResults("Attendance Submission Failed!", "You have already submitted attendance on this device");
                 }
                 @Override
                 public void onFailure(Call<ModelSerializer> call, Throwable t) {
-                    receiveUtil.showAttendanceResults("Attendance Submitted!" , "");
+                    showAttendanceResults("Attendance Submitted!" , "");
                 }
             });
         }, error-> {
         });
+    }
+
+    private void showAttendanceResults(String title, String message){
+        ActivityUtil activityUtil = new ActivityUtil(context);
+        new AlertDialog.Builder(ReceiveActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, (dialog,which) -> activityUtil.goToSignIn())
+                .create()
+                .show();
     }
 
 }

app/src/main/java/com/setsuna/client/quiet/signInActivity.java
@@ -9,12 +9,12 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
-import com.setsuna.client.quiet.util.AccountManager;
+import com.setsuna.client.quiet.util.manager.AccountManager;
 import com.setsuna.client.quiet.util.CheckBackend;
 
 public class signInActivity extends AppCompatActivity{
-    private Context context;
 
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){

app/src/main/java/com/setsuna/client/quiet/student/TransmitActivity.java
@@ -3,7 +3,6 @@ package com.setsuna.client.quiet.student;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,9 +14,7 @@ import android.widget.Toast;
 
 
 import com.setsuna.client.quiet.R;
-import com.setsuna.client.quiet.signInActivity;
 import com.setsuna.client.quiet.util.ActivityUtil;
-import com.setsuna.client.quiet.util.TransmitUtil;
 
 import org.quietmodem.Quiet.FrameTransmitter;
 import org.quietmodem.Quiet.FrameTransmitterConfig;
@@ -28,11 +25,7 @@ import java.io.IOException;
 
 public class TransmitActivity extends AppCompatActivity {
 
-    private Context context;
-    private FrameTransmitter transmitter;
-    private FrameTransmitterConfig transmitterConfig;
-    private TransmitUtil transmitUtil = new TransmitUtil(context);
-    private ActivityUtil activityUtil = new ActivityUtil(context);
+    private  Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +33,8 @@ public class TransmitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_transmit);
     }
 
-    public TransmitActivity(Context context){
-        this.context = context;
-    }
+
+
 
     // 9 April 2017: This is really bad code and we should do something about it
     // Let's see how long this stays here....
@@ -52,11 +44,13 @@ public class TransmitActivity extends AppCompatActivity {
         if (payload.matches("")) {
             Toast.makeText(getApplicationContext(), "You did not enter a code!", Toast.LENGTH_LONG).show();
         } else {
-            transmitUtil.changeVolume();
+            changeVolume();
+            hideKeyboard();
             try {
-                transmitterConfig = new FrameTransmitterConfig(context, context.getResources().getString(R.string.quiet_profile));
+                FrameTransmitterConfig transmitterConfig;
+                FrameTransmitter transmitter;
+                transmitterConfig = new FrameTransmitterConfig(TransmitActivity.this, TransmitActivity.this.getResources().getString(R.string.quiet_profile));
                 transmitter = new FrameTransmitter(transmitterConfig);
-                transmitUtil.hideKeyboard();
                 (findViewById(R.id.layout_code_input)).setVisibility(ScrollView.GONE);
                 (findViewById(R.id.layout_code_broadcasting)).setVisibility(ScrollView.VISIBLE);
                 new Thread(() -> {
@@ -67,19 +61,17 @@ public class TransmitActivity extends AppCompatActivity {
                         } catch (IOException e) {
                             System.out.println("our message might be too long or the transmit queue full");
                         }
-                        transmitUtil.timeDelay(1000);
+                       timeDelay(1000);
                     }
                 }).start();
             } catch (ModemException me) {
-                new AlertDialog.Builder(context)
+                new AlertDialog.Builder(TransmitActivity.this)
                         .setTitle(R.string.error)
                         .setIcon(R.drawable.ic_error_outline_black_24px)
                         .setMessage(R.string.modem_exception)
                         .setCancelable(false)
                         .setPositiveButton(android.R.string.ok, (dialog,which) ->
-                        {
-                            finish();
-                        })
+                                signIn())
                         .create()
                         .show();
             } catch (IOException ioe) {
@@ -88,9 +80,12 @@ public class TransmitActivity extends AppCompatActivity {
         }
     }
 
-    public void stopBroadcast(View view){
+    private void signIn(){
+        finish();
+        ActivityUtil activityUtil = new ActivityUtil(context);
         activityUtil.goToSignIn();
     }
+
 
     public void onDestroy() {
         super.onDestroy();
@@ -101,4 +96,23 @@ public class TransmitActivity extends AppCompatActivity {
         super.onPause();
         finish();
     }
+
+    private void timeDelay(long t){
+        try {
+            Thread.sleep(t);
+        } catch (InterruptedException e) {
+            System.out.println("got interrupted!");
+        }
+    }
+
+    private void hideKeyboard () {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+    }
+
+    private void changeVolume(){
+        AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 30, 0);
+    }
+
 }

app/src/main/java/com/setsuna/client/quiet/util/ActivityUtil.java
@@ -12,7 +12,6 @@ import com.setsuna.client.quiet.student.TransmitActivity;
 public class ActivityUtil {
 
     private Context context;
-    private Activity activity;
 
     public ActivityUtil(Context context){
         this.context = context;
@@ -21,19 +20,19 @@ public class ActivityUtil {
     public void goToSignIn(){
         Intent signInIntent = new Intent(context, signInActivity.class);
         context.startActivity(signInIntent);
-        activity.finish();
+        ((Activity)context).finish();
     }
 
     public void goToTransmit(){
         Intent transmitIntent = new Intent(context, TransmitActivity.class);
         context.startActivity(transmitIntent);
-        activity.finish();
+        ((Activity)context).finish();
     }
 
     public void goToReceive(){
         Intent receiveIntent = new Intent(context, ReceiveActivity.class);
         context.startActivity(receiveIntent);
-        activity.finish();
+        ((Activity)context).finish();
     }
 
 }

app/src/main/java/com/setsuna/client/quiet/util/ReceiveUtil.java
@@ -1,34 +1,0 @@
-package com.setsuna.client.quiet.util;
-
-
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AlertDialog;
-
-import com.setsuna.client.quiet.signInActivity;
-
-public class ReceiveUtil {
-
-    private Context context;
-    private Activity activity;
-    private ActivityUtil activityUtil = new ActivityUtil(context);
-
-
-    public ReceiveUtil(Context context){
-        this.context = context;
-    }
-
-    public void showAttendanceResults(String title, String message){
-        new AlertDialog.Builder(context)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton(android.R.string.ok, (dialog,which) -> {
-                    activity.finish();
-                    activityUtil.goToSignIn();
-                })
-                .create()
-                .show();
-    }
-
-}

app/src/main/java/com/setsuna/client/quiet/util/TransmitUtil.java
@@ -1,35 +1,0 @@
-package com.setsuna.client.quiet.util;
-
-
-import android.app.Activity;
-import android.content.Context;
-import android.media.AudioManager;
-import android.view.inputmethod.InputMethodManager;
-
-public class TransmitUtil{
-
-    private Context context;
-
-    public TransmitUtil(Context context){
-        this.context = context;
-    }
-
-    public void timeDelay(long t) {
-        try {
-            Thread.sleep(t);
-        } catch (InterruptedException e) {
-            System.out.println("got interrupted!");
-        }
-    }
-
-    public void hideKeyboard () {
-        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
-    }
-
-    public void changeVolume(){
-        AudioManager audioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 30, 0);
-    }
-
-}

app/src/main/java/com/setsuna/client/quiet/util/manager/AccountManager.java
@@ -1,15 +1,13 @@
-package com.setsuna.client.quiet.util;
+package com.setsuna.client.quiet.util.manager;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 
 import com.setsuna.client.quiet.R;
-import com.setsuna.client.quiet.lecturer.ReceiveActivity;
-import com.setsuna.client.quiet.student.TransmitActivity;
+import com.setsuna.client.quiet.util.ActivityUtil;
 
 
 public class AccountManager extends AsyncTask<String, Integer, String> {
@@ -19,14 +17,13 @@ public class AccountManager extends AsyncTask<String, Integer, String> {
     // Can be replaced with actual connection code
 	
     public static String loggedInUserID;
-
+    private Activity activity;
     private ProgressDialog progressDialog;
     private Context globalContext;
     private String result;
     private SignInResponse signInState;
     private SignInType signInType;
     private String connectionType;
-    private ActivityUtil activityUtil = new ActivityUtil(globalContext);
 
     public AccountManager(Context context) {
         globalContext = context;
@@ -82,6 +79,7 @@ public class AccountManager extends AsyncTask<String, Integer, String> {
     protected void onPostExecute(String result1) {
             if (progressDialog.isShowing()) {
                 progressDialog.dismiss();
+                ActivityUtil activityUtil = new ActivityUtil(globalContext);
 
                 if (connectionType.equals("SignInOnly")) {
                     if (signInState.equals(SignInResponse.SignedIn)) {

app/src/main/java/com/setsuna/client/quiet/util/manager/ApiManager.java
@@ -1,4 +1,6 @@
-package com.setsuna.client.quiet.util;
+package com.setsuna.client.quiet.util.manager;
+
+import com.setsuna.client.quiet.util.ModelSerializer;
 
 import retrofit2.Call;
 import retrofit2.http.Field;

app/src/main/java/com/setsuna/client/quiet/util/manager/PermissionManager.java
@@ -1,4 +1,4 @@
-package com.setsuna.client.quiet.util;
+package com.setsuna.client.quiet.util.manager;
 
 
 import android.Manifest;

app/src/main/res/layout/activity_receive.xml
@@ -19,7 +19,7 @@
         android:layout_alignParentTop="true"
         android:layout_centerHorizontal="true"
         android:paddingBottom="16dp"
-        android:text="Searching for lecturer device"
+        android:text="Searching for students' device"
         android:textAlignment="center"
         android:textAppearance="?android:attr/textAppearanceLarge" />
 

app/src/main/res/layout/activity_transmit.xml
@@ -1,99 +1,27 @@
 <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
     android:layout_width="match_parent"
     android:layout_height="match_parent"
-    android:orientation="vertical"
-    android:padding="16dp">
+    android:orientation="vertical">
 
-    <ScrollView xmlns:android="http://schemas.android.com/apk/res/android"
-        android:id="@+id/layout_code_input"
+    <com.github.ybq.android.spinkit.SpinKitView
+        xmlns:app="http://schemas.android.com/apk/res-auto"
+        android:id="@+id/spin_kit"
+        style="@style/SpinKitView.Large.ThreeBounce"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_gravity="center"
+        app:SpinKit_Color="@color/colorAccent" />
+
+    <TextView
+        android:id="@+id/text_status"
         android:layout_width="match_parent"
-        android:layout_height="match_parent"
-        android:layout_gravity="center_horizontal"
-        android:visibility="visible" >
+        android:layout_height="wrap_content"
+        android:layout_alignParentTop="true"
+        android:layout_centerHorizontal="true"
+        android:paddingBottom="16dp"
+        android:text="Searching for lecturer's device"
+        android:textAlignment="center"
+        android:textAppearance="?android:attr/textAppearanceLarge" />
 
-        <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-            android:layout_width="match_parent"
-            android:layout_height="match_parent"
-            android:orientation="vertical" >
-
-            <TextView
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:layout_gravity="center_horizontal"
-                android:text="@string/label_prompt_code"
-                android:textAppearance="?android:attr/textAppearanceMedium" />
-
-            <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:layout_gravity="center_horizontal|bottom">
-
-                <EditText
-                    android:id="@+id/textCode"
-                    android:layout_width="match_parent"
-                    android:layout_height="wrap_content"
-                    android:layout_gravity="bottom"
-                    android:layout_weight="1"
-                    android:digits="1234567890"
-                    android:inputType="number" />
-
-                <Button
-                    android:id="@+id/button_send"
-                    android:layout_width="wrap_content"
-                    android:layout_height="wrap_content"
-                    android:layout_gravity="center_horizontal|bottom"
-                    android:onClick="startBroadcast"
-                    android:text="@string/broadcast" />
-
-            </LinearLayout>
-
-            <!-- Not using fragments yet, doing cheap visibility change -->
-
-        </LinearLayout>
-
-    </ScrollView>
-
-    <ScrollView xmlns:android="http://schemas.android.com/apk/res/android"
-        android:id="@+id/layout_code_broadcasting"
-        android:layout_width="match_parent"
-        android:layout_height="match_parent"
-        android:layout_gravity="center_horizontal"
-        android:visibility="gone" >
-
-        <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-            android:layout_width="match_parent"
-            android:layout_height="match_parent"
-            android:orientation="vertical" >
-
-            <com.github.ybq.android.spinkit.SpinKitView
-                xmlns:app="http://schemas.android.com/apk/res-auto"
-                android:id="@+id/spin_kit"
-                style="@style/SpinKitView.Large.ThreeBounce"
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:layout_gravity="center"
-                app:SpinKit_Color="@color/colorAccent" />
-
-            <TextView
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:layout_gravity="center_horizontal"
-                android:paddingTop="16dp"
-                android:text="@string/action_broadcasting"
-                android:textAlignment="center"
-                android:textAppearance="@android:style/TextAppearance.Large" />
-
-            <Button
-                android:id="@+id/button_stop"
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:layout_gravity="center_horizontal|bottom"
-                android:onClick="stopBroadcast"
-                android:text="@string/stop_broadcast" />
-
-
-        </LinearLayout>
-
-    </ScrollView>
 
 </LinearLayout>

app/src/main/res/values/activity_signin.xml
@@ -1,2 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<resources></resources>